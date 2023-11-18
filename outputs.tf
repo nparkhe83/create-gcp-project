aa4901739a7380c02611dd39aaa6205d22f84579
Nilesh <parkhenilesh@outlook.com>
2023-11-18 22:09:47 +0530

    JSD017 - Remove own backend creation from Config

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "poc_project_id" {
   value = local.project_id
 }
+
+output "poc_backend_bucket_id" {
+  value = google_storage_bucket.poc-tfstate-bucket.name
+}
--- storage.tf
+++ storage.tf
@@ -1,14 +1,3 @@
-resource "google_storage_bucket" "admin-tfstate-bucket" {
-  name          = "admin-tfstate-bucket"
-  location      = "US"
-  force_destroy = true // To allow removal of a non-empty bucket
-
-  uniform_bucket_level_access = true
-  versioning {
-    enabled = true
-  }
-}
-
 resource "google_storage_bucket" "poc-tfstate-bucket" {
   name          = local.poc_storage_bucket_name
   location      = "US"
